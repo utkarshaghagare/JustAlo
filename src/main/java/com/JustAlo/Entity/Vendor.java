2fee01459c878fc681591d8ee29b61de3a344735
Utkarsha Ghagare <utkarshaghagare03@gmail.com>
2024-07-15 16:18:25 +0530

    Initial Commit

---
+++ src/main/java/com/JustAlo/Entity/Vendor.java
@@ -0,0 +1,39 @@
+package com.JustAlo.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String phone_number;
+    private String organization_name;
+    private String address;
+    private String doc1;
+    private String doc2;
+    private String password;
+    private Boolean verification_status;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_ROLE", joinColumns = {
+            @JoinColumn(name = "USER_ID", referencedColumnName = "username")
+    },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "ROLE_ID")
+            }
+
+    )
+    private Set<Role> Role;
+
+}
